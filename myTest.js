dbee1d3d476fc5cfee1f2835d7e451d94ed16b7a
Guyu <1902810823@qq.com>
2023-03-07 10:47:34 +0800

    just test

---
+++ myTest.js
@@ -0,0 +1,19 @@
+let b = {
+    start: 0,
+    end: 100,
+    async next () {
+      if (this.start < this.end) {
+        return {value: this.satrt + 100, done: false}
+      }
+      return {value: this.start, done: true}
+    },
+    [Symbol.asyncIterator] () {
+      return this
+    }
+  }
+  
+  (async function () {
+    for await (let j of b) {
+      console.log(j)
+    }
+  })()
